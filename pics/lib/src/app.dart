b3d69e5b7357c2676f593f82281a0b2146eebbb6
mpilopillz <mpilo.dlamini@yahoo.com>
2022-06-07 17:53:51 +0200

    learning async await

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -10,19 +11,19 @@ class AppState extends State<App> {
   // this is my state / instance variable that changes over time
   int counter = 0;
 
+  void fetchImage() async {
+    counter++;
+    // not the actual json data. this is information about the request.
+    var response = await get(Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+  }
+
   // Build is called on every rerender of each widget
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('$counter'),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 10;
-            });
-          },
-        ),
+        floatingActionButton:
+            FloatingActionButton(child: Icon(Icons.add), onPressed: fetchImage),
         appBar: AppBar(
           title: Text('Lets see some images!'),
         ),
