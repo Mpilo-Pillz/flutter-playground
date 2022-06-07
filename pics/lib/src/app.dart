040c62cf0d8d3b90fca4627d2444def85d4780f6
mpilopillz <mpilo.dlamini@yahoo.com>
2022-06-07 18:04:47 +0200

    storing images in a list

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
+
+import 'models/image_model.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -10,11 +14,20 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   // this is my state / instance variable that changes over time
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter++;
     // not the actual json data. this is information about the request.
-    var response = await get(Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    var response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    var imageModel = ImageModel.fromJson(json.decode(
+        response.body)); // remember the JSON is raw needs to be converted
+
+    // update when the images come
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   // Build is called on every rerender of each widget
