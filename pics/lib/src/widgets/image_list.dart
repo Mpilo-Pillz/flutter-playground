e09a5818c6fe018258514ab853ca4fd4a9af579a
mpilopillz <mpilo.dlamini@yahoo.com>
2022-06-08 18:53:51 +0200

    adding image list toi render images

---
+++ pics/lib/src/widgets/image_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+// this will be a stateless image because this widget is not changing the state. the state will be changed by the app widget
+class ImageList extends StatelessWidget {
+  // final List<ImageModel> images;
+  final List images;
+
+  ImageList(this.images);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Image.network(images[index].url);
+      },
+    );
+  }
+}
